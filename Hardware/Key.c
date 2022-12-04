89e866909e6e3a671d117c454950173634f9098e
Xiayuan <1615639948@qq.com>
2022-12-05 00:56:25 +0800

    Improved SD read and write (no file manipulation)
    可以将AD转换的值写入数组放入SD卡读取，但是问题有几个：1.只能写256大小的数组一次进入SD卡，并且每次都会覆盖重复的区域没法连接，没法做到手动停止传输这一操作。2.没有进行文件操作，写入的数量实在有限。

--- Hardware/Key.c
+++ Hardware/Key.c
@@ -7,7 +7,7 @@ void Key_Init(void)
 	
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_11;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
@@ -21,6 +21,13 @@ uint8_t Key_GetNum(void)
 		while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0);
 		Delay_ms(20);
 		KeyNum = 1;
+	}
+	if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11) == 0)
+	{
+		Delay_ms(20);
+		while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11) == 0);
+		Delay_ms(20);
+		KeyNum = 2;
 	}
 	return KeyNum;
 }
